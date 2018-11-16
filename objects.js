376820de3976358813290b6e8d90652d54d29b31
dekc89 <dekc89@gmail.com>
2018-11-16 13:41:26 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,3 +2,8 @@ var playlist = {
   Korn: "Blind",
   Slipknot: "Duality"
 };
+
+function updatePlaylist(obj, artist, song) {
+  var newPlay = Object.assign({artist:song}, obj);
+  return newPlay;
+}
