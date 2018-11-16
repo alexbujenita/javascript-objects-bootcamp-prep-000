ad3a53321fd8171295decbbf54c9ae40aa91bba2
dekc89 <dekc89@gmail.com>
2018-11-16 14:24:31 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,3 +7,8 @@ function updatePlaylist(obj, artist, song) {
   var newPlay = Object.assign(obj, {[artist]:song});
   return newPlay;
 }
+
+function removeFromPlaylist(obj, art) {
+  var del = delete obj.art;
+  return del;
+}
