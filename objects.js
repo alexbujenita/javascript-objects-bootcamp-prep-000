85a266e2fa7c4fd98c7c33007289c407fd35a9b2
dekc89 <dekc89@gmail.com>
2018-11-16 14:20:16 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,6 +4,6 @@ var playlist = {
 };
 
 function updatePlaylist(playlist, artist, song) {
-  return  Object.assign({}, playlist, {[artist]:song});
-
+  playlist = Object.assign({}, playlist, {[artist]:song});
+  return playlist;
 }
